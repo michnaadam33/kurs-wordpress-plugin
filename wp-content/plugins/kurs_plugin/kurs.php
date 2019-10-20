2817e9a2b4e174c80240b8ef41377581c3eb1cb6
Adam Michna <michnaadam33@gmail.com>
2019-10-20 14:27:57 +0200

    l3

--- wp-content/plugins/kurs_plugin/kurs.php
+++ wp-content/plugins/kurs_plugin/kurs.php
@@ -6,3 +6,17 @@ Plugin URI: https://www.example.com
 Author: Adam Michna
 Version: 0.0.2
 */
+
+class KursPlugin {
+
+    public function __construct()
+    {
+        add_filter('the_content', [$this, 'theContentFilter']);
+    }
+
+    public function theContentFilter($content){
+        return $content . '<p>Dziękujemy, zapraszamy ponownie.</p>';
+    }
+}
+
+$kursPlugin = new KursPlugin();
