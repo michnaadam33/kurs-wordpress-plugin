a2c1aa862428d00c2cd693fc4929a98f858c7e80
Adam Michna <michnaadam33@gmail.com>
2019-11-11 16:31:19 +0100

    l6

--- wp-content/plugins/kurs_plugin/KursPluginSettings.php
+++ wp-content/plugins/kurs_plugin/KursPluginSettings.php
@@ -6,6 +6,15 @@ class KursPluginSettings
     public function __construct()
     {
         add_action('admin_menu', [$this, 'addAdminPage']);
+        add_action('admin_init', [$this, 'registerOption']);
+    }
+
+    public function registerOption()
+    {
+        add_option( 'kurs_plugin_cytat');
+        add_option( 'kurs_plugin_cytat2');
+        register_setting( 'kurs_plugin_group', 'kurs_plugin_cytat');
+        register_setting( 'kurs_plugin_group', 'kurs_plugin_cytat2');
     }
 
     public function addAdminPage()
@@ -21,6 +30,32 @@ class KursPluginSettings
 
     public function pageRender()
     {
-        echo "TODO";
+       ?>
+        <div class="wrap">
+            <h2>Opcje konfiguracyjne</h2>
+            <form method="post" action="options.php">
+                <?php settings_fields( 'kurs_plugin_group' ); ?>
+                <table>
+                    <tr valign="top">
+                        <th scope="row">
+                            <label for="kurs_plugin_cytat">Cytat:</label>
+                        </th>
+                        <td>
+                            <input type="text" id="kurs_plugin_cytat" name="kurs_plugin_cytat"
+                                   value="<?= get_option('kurs_plugin_cytat'); ?>" />
+                        </td>
+                        <th scope="row">
+                            <label for="kurs_plugin_cytat2">Cytat2:</label>
+                        </th>
+                        <td>
+                            <input type="text" id="kurs_plugin_cytat2" name="kurs_plugin_cytat2"
+                                   value="<?= get_option('kurs_plugin_cytat2'); ?>" />
+                        </td>
+                    </tr>
+                </table>
+                <?php submit_button(); ?>
+            </form>
+        </div>
+        <?php
     }
 }
