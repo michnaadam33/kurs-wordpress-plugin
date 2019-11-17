f63abd8ddacf095a8a521a826166314cdf16ccc0
Adam Michna <michnaadam33@gmail.com>
2019-11-17 11:11:27 +0100

    l7

--- wp-content/plugins/kurs_plugin/KursPluginSettings.php
+++ wp-content/plugins/kurs_plugin/KursPluginSettings.php
@@ -12,9 +12,7 @@ class KursPluginSettings
     public function registerOption()
     {
         add_option( 'kurs_plugin_cytat');
-        add_option( 'kurs_plugin_cytat2');
         register_setting( 'kurs_plugin_group', 'kurs_plugin_cytat');
-        register_setting( 'kurs_plugin_group', 'kurs_plugin_cytat2');
     }
 
     public function addAdminPage()
@@ -34,24 +32,22 @@ class KursPluginSettings
         <div class="wrap">
             <h2>Opcje konfiguracyjne</h2>
             <form method="post" action="options.php">
-                <?php settings_fields( 'kurs_plugin_group' ); ?>
-                <table>
-                    <tr valign="top">
+                <?php
+                settings_fields( 'kurs_plugin_group' );
+                $optionsCytat = get_option('kurs_plugin_cytat');
+                ?>
+                <table class="kurs-plugin-option-table">
+                    <?php foreach ($optionsCytat as $key => $option): ?>
+                    <tr valign="top" id="option-id-<?=$key?>" class="kurs-plugin-option-tr">
                         <th scope="row">
-                            <label for="kurs_plugin_cytat">Cytat:</label>
+                            <label for="kurs_plugin_cytat<?= $key ?>"><?=$key?>:</label>
                         </th>
                         <td>
-                            <input type="text" id="kurs_plugin_cytat" name="kurs_plugin_cytat"
-                                   value="<?= get_option('kurs_plugin_cytat'); ?>" />
-                        </td>
-                        <th scope="row">
-                            <label for="kurs_plugin_cytat2">Cytat2:</label>
-                        </th>
-                        <td>
-                            <input type="text" id="kurs_plugin_cytat2" name="kurs_plugin_cytat2"
-                                   value="<?= get_option('kurs_plugin_cytat2'); ?>" />
+                            <input type="text" id="kurs_plugin_cytat<?= $key ?>" name="kurs_plugin_cytat[<?=$key?>]"
+                                   value="<?= get_option('kurs_plugin_cytat')[$key]; ?>" />
                         </td>
                     </tr>
+                    <?php endforeach; ?>
                 </table>
                 <?php submit_button(); ?>
             </form>
